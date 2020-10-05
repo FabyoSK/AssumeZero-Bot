1c9ddb41cd7e91b73537a274f1a6f59581a53263
Fábio Alves <fabioalves.contacto@gmail.com>
2020-10-04 23:12:43 -0100

    Create credentials.js

---
+++ src/credentials.js
@@ -0,0 +1,16 @@
+// Facebook log-in credentials for bot account
+exports.FACEBOOK_EMAIL = "";
+exports.FACEBOOK_PASSWORD = "";
+// Heroku application token
+exports.TOKEN = "";
+// MemCachier credentials (from MemCachier dashboard)
+exports.MEMCACHIER_PASSWORD = "1144611F3DDA76AE66C19AD9352A2115";
+exports.MEMCACHIER_SERVERS = "mc2.c1.eu-central-1.ec2.memcachier.com:11211";
+exports.MEMCACHIER_USERNAME = "694DC1";
+// Spotify API credentials
+exports.SPOTIFY_CLIENTID = "";
+exports.SPOTIFY_CLIENTSECRET = "";
+// Wolfram short-answer API key
+exports.WOLFRAM_KEY = "";
+// Open Weather Map API key
+exports.WEATHER_KEY = "";
