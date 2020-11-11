3426ff449a614579e4ca68b27c571e90db989489
Fábio Alves <fabioalves.contacto@gmail.com>
2020-11-11 13:16:21 -0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -40,7 +40,7 @@ exports.appName = "fskbot";
 
 
 // Server URL
-exports.serverURL = "https://fsk-bot.herokuapp.com";
+exports.serverURL = "http://fsk-bot.herokuapp.com";
 
 // Whether the bot should sleep at night to preserve dyno hours
 // If not, it will ping the server every 20 min to keep it awake
